5415708f41adbe308e7a5e1d1dab8d8e2d71e058
klewandowski <bikeholik@gmail.com>
2016-02-29 15:10:51 +0100

    Initial project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # schere-stein-papier
+
+Sample project implementing chinese [game](https://de.wikipedia.org/wiki/Schere,_Stein,_Papier)
---
+++ src/main/java/com/github/bikeholik/ssp/Application.java
@@ -0,0 +1,17 @@
+package com.github.bikeholik.ssp;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .logStartupInfo(false)
+                .run(args);
+    }
+}
